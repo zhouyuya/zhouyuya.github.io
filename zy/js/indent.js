b43b46babe268e3c8102743159768e4c4fecf813
周宇 <zhou.yu@seatell.cn>
2018-05-06 16:19:06 +0800

    ''

--- zy/js/indent.js
+++ zy/js/indent.js
@@ -2,16 +2,24 @@
 $(document).ready(function (){
 
     var oPay=$('.go').eq(0);
-
+    var AaddressCard=$('.address_card');
+    var i=0;
 
 
     //点击付款
     oPay.click(function(){
-        window.location.href='indent.html';
+        window.location.href='indent_success.html';
 
     })
 
-
+    //点击选择地址
+     for(i=0;i<AaddressCard.length;i++)
+    {
+        AaddressCard.click(function (){
+            k=$(this).index();
+            $(this).addClass('selected').parents('li').siblings('li').children('a').removeClass('selected');
+        });
+    }
 
 });
 
