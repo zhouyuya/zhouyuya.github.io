99bdd29cda26ae01e33d1735908e2a956366524f
周宇 <zhou.yu@seatell.cn>
2018-06-06 11:51:09 +0800

    ''

--- a-web-test/detail/index.js
+++ a-web-test/detail/index.js
@@ -45,7 +45,7 @@ function play (json){
   document.querySelector("#main").innerHTML=` <p class="title">${json.title?json.title:json.discribe || '趣发生'}</p>
     <p class="location">${json.fenceCenterAddress}</p>
     <p class="time">${json.pageView} 次播放</p>`;
-  document.title=json.title?json.title:json.discribe || '趣发生';
+  document.title=json.title?json.title:json.describe || '趣发生';
   var player = videojs('example-video');
   player.play();
 }
